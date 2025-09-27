f018f7fcfa169c9dce9f7ca8e4cbabd77b8e5bda
Teodorescu Andrei-Calin <andrei-calin.teodorescu@ing.com>
2025-09-27 11:53:48 +0300

    Added settings for github authentication

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,2 @@
+export { GET, POST } from "@/auth";
+//these are some endpoints that an external server (OAuth server) will call to handle authentication
---
+++ src/auth.ts
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/db";
+
+const GITHUB_CLINET_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+if (!GITHUB_CLINET_ID || !GITHUB_CLIENT_SECRET) {
+  throw new Error("Missing env varibales");
+}
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signOut,
+  signIn,
+} = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [
+    GitHub({
+      clientId: GITHUB_CLINET_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      authorization: { params: { scope: "read:user" } },
+    }),
+  ],
+  callbacks: {
+    //usually not needed, fixing a bug in next-auth
+    async session({ session, user }: any) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+});
