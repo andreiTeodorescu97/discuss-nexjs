1e138e93aab35cf08b6d19f11a34c1caac5d664c
Teodorescu Andrei-Calin <andrei-calin.teodorescu@ing.com>
2025-09-30 13:51:03 +0300

    Section 9: added request memoization to comments components

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,5 +1,6 @@
 import type { Comment } from "@prisma/client";
 import { db } from "@/db";
+import { cache } from "react";
 
 export type CommentWithAuthor = Comment & {
   user: {
@@ -8,11 +9,13 @@ export type CommentWithAuthor = Comment & {
   };
 };
 
-export async function fetchCommentsByPostId(
-  postId: string
-): Promise<CommentWithAuthor[]> {
-  return db.comment.findMany({
-    where: { postId: postId },
-    include: { user: { select: { name: true, image: true } } }
-  });
-}
+//function that we want to memoize
+export const fetchCommentsByPostId = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
+    console.log("Fetching comments for postId:", postId);
+    return db.comment.findMany({
+      where: { postId: postId },
+      include: { user: { select: { name: true, image: true } } },
+    });
+  }
+);
